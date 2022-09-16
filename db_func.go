d0ce2b92ea8e33ff942a6667b393f0e27e5300a4
Very Smooth Man <40175264+ManSmooth@users.noreply.github.com>
2022-09-16 20:38:57 +0700

    Insert Row Fix Attempt

--- db_func.go
+++ db_func.go
@@ -9,11 +9,22 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+
 func createConnectionToDatabase() *sql.DB {
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("DBUSER"), os.Getenv("DBPASSWORD"), os.Getenv("DBHOST"), os.Getenv("DBPORT"), os.Getenv("DB")))
-	if err != nil {
-		panic(err)
+	if db == nil {
+		var err error
+		db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("DBUSER"), os.Getenv("DBPASSWORD"), os.Getenv("DBHOST"), os.Getenv("DBPORT"), os.Getenv("DB")))
+		if err != nil {
+			panic(err)
+		}
+		db.SetMaxOpenConns(20)
+		db.SetMaxIdleConns(20)
+		db.SetConnMaxLifetime(time.Minute * 5)
 	}
-	db.SetConnMaxLifetime(time.Minute * 3)
+	pingErr := db.Ping()
+	if pingErr != nil {
+		panic(pingErr)
+	}
 	return db
 }
